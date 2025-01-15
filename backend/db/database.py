1357f2e92e797c75ec5f2431c9322554f83da18c
LukV <luk.verhelst@gmail.com>
2025-01-15 10:11:32 +0100

    Migrate sqlite to PG17

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,16 +1,14 @@
 import os
-from pathlib import Path
-import dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-env_path = Path(__file__).resolve().parent / ".env"
-dotenv.load_dotenv(dotenv_path=env_path)
+# PostgreSQL database connection URL
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "sqlite:///./test.db")
+# Initialize the engine
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-# Initialize the engine and base
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# Configure SessionLocal and Base
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
